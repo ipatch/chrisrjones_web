66cf1040a1cf4f0bd31d6f12c78e3a71c0cb53ea
ipatch <chris.r.jones.1983@gmail.com>
2015-01-06 14:37:48 -0600

    move new article link above hr tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,33 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		# @user= User.find(params[:id])
+		@user = User.find(params[:id])
+
+    	respond_to do |format|
+      		# if @user.update_attributes(params[:user])
+      		if @user.update_attributes(user_params)
+        		format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        		format.json { head :no_content }
+      		else
+        		format.html { render action: "edit" }
+        		format.json { render json: @user.errors, status: :unprocessable_entity }
+      		end
+    	end
+	end
+
+	def destroy
+		 session[:user_id] = nil
+    	@user = User.find(params[:id])
+    	@user.destroy
+    	redirect_to :controller=>'users', :action => 'new'
+	end
+
 	private
 		def user_params
 			params.require(:user).permit(:email, :password, :password_confirmation)
