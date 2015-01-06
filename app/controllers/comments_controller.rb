d3bc8e4b69421a2d93031efcbd538bdbfb55e17a
ipatch <chris.r.jones.1983@gmail.com>
2015-01-06 12:03:35 -0600

    fix comments, should be able to add / destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,20 @@ class CommentsController < ApplicationController
 
 	before_filter :authorize, only: [:create, :edit, :update, :destroy]
 
+	skip_before_filter :verify_authenticity_token
+
 	def create
-		@article = Article.find(params[:article_id])
+		@article = Article.find_by_slug(params[:article_id])
 		@comment = @article.comments.create(comment_params)
 		redirect_to article_path(@article)
 	end
 
 	def destroy
-    	@article = Article.find(params[:article_id])
-    	@comment = @article.comments.find(params[:id])
-    	@comment.destroy
+    	@article = Article.find_by_slug(params[:article_id])
+    	if current_user == @article.user || current_user.admin_user == true
+    		@comment = @article.comments.find(params[:id])
+    		@comment.destroy
+    	end
     	redirect_to article_path(@article)
   	end
 
