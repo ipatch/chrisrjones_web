2beceab3a8838ceeea81a83f9067a6a230455742
ipatch <chris.r.jones.1983@gmail.com>
2015-01-07 12:53:34 -0600

    only user can update their info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,23 +25,27 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		# if session[:user_id] == @user = User.find(params[id])
 		@user = User.find(params[:id])
 	end
 
 	def update
 		# @user= User.find(params[:id])
 		@user = User.find(params[:id])
-
-    	respond_to do |format|
-      		# if @user.update_attributes(params[:user])
-      		if @user.update_attributes(user_params)
-        		format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        		format.json { head :no_content }
-      		else
-        		format.html { render action: "edit" }
-        		format.json { render json: @user.errors, status: :unprocessable_entity }
+		if session[:user_id] == @user.id || current_user.admin_user == true
+			respond_to do |format|
+      			# if @user.update_attributes(params[:user])
+      			if @user.update_attributes(user_params)
+        			format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        			format.json { head :no_content }
+      			else
+        			format.html { render action: "edit" }
+        			format.json { render json: @user.errors, status: :unprocessable_entity }
+      			end
       		end
-    	end
+      	else
+      		redirect_to users_url, notice: "Can not edit this user"
+      	end
 	end
 
 	def destroy
