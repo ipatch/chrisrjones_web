7a353937fe353b62b1a8b256b478b08d9c188730
ipatch <chris.r.jones.1983@gmail.com>
2015-01-12 14:02:37 -0600

    password reset func should be working NOW!

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,16 +10,24 @@ class PasswordResetsController < ApplicationController
 
   def edit
   	@user = User.find_by_password_reset_token!(params[:id])
+  	# @user = User.find_by_password_reset_token!(password_params)
   end
 
   def update
   	@user = User.find_by_password_reset_token!(params[:id])
   	if @user.password_reset_sent_at < 2.hours.ago
   		redirect_to new_password_reset_path, :alert => "Password reset has expired."
-  	elseif @user.update_attributes(params[:user])
+  	# elseif @user.update_attributes(params[:user])
+  	elsif @user.update_attributes(password_params)
   		redirect_to root_url, :notice => "Password has been reset!"
   	else
   		render :edit
   	end
   end
+
+  private
+	def password_params
+		params.require(:user).permit(:password, :password_confirmation)
+	end
+
 end
