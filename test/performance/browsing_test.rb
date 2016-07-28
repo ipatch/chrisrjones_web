b34dc207d0c486adf15e9f50fb911b91f43c3a16
ipatch <chris.r.jones.1983@gmail.com>
2016-07-28 15:22:53 -0500

    comment out browser test

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 # http://stackoverflow.com/a/28518943/708807
 # require 'rails/performance_test_help'
 
-class BrowsingTest < ActionDispatch::PerformanceTest
-  # Refer to the documentation for all available options
-  # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
-  #                          :output => 'tmp/performance', :formats => [:flat] }
+# class BrowsingTest < ActionDispatch::PerformanceTest
+#   # Refer to the documentation for all available options
+#   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
+#   #                          :output => 'tmp/performance', :formats => [:flat] }
 
-  def test_homepage
-    get '/'
-  end
-end
+#   def test_homepage
+#     get '/'
+#   end
+# end
