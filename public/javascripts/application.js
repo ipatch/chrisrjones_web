5f1864c294ccaa292d1b3ee731f9d31bc221ec65
Chris <chris.r.jones.1983@gmail.com>
2018-02-06 00:02:27 -0600

    Delete application.js

--- public/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-// place your application-specific Javascript functions and classes here
-// this file is automatically included by javascript_include_tag: defaults
-
-function jsRoar() {
-	alert('I am Javascript. Hear me roar!');
-}
