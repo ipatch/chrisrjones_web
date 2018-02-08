b4744c9f99c11efb1abbdc485b8eec1054a74668
ipatch <chris.r.jones.1983@gmail.com>
2018-02-07 22:41:07 -0600

    The unfacts, did we have them, are too imprecisely few to warrant our certitude.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -31,32 +31,6 @@ role  :db,    %w{deploy@107.170.40.252}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-
 set :ssh_options, {
    keys: %w(/home/deploy/.ssh/id_rsa),
    forward_agent: true,
