b852229f407c00047b73a090cc4bbeb02b8c21e7
ipatch <chris.r.jones.1983@gmail.com>
2018-02-07 22:31:41 -0600

    Relations are simply a tedious pack of people, who haven't the remotest knowledge of how to live, nor the smallest instinct about when to die. 		-- Oscar Wilde, "The Importance of Being Earnest"

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,6 +74,17 @@ namespace :puma do
 end
 
 namespace :deploy do
+  desc "Check that we can access everything"
+  task :check_write_permissions do
+    on roles(:all) do |host|
+      if test("[ -w #{fetch(:deploy_to)} ]")
+        info "#{fetch(:deploy_to)} is writable on #{host}"
+      else
+        error "#{fetch(:deploy_to)} is not writable on #{host}"
+      end
+    end
+  end
+
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
