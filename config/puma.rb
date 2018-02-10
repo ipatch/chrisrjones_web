572346bd25ea9af218d0a6509c3c436d12eb7b80
ipatch <chris.r.jones.1983@gmail.com>
2018-02-10 15:45:38 -0600

    Two heads are more numerous than one.

--- config/puma.rb
+++ config/puma.rb
@@ -17,14 +17,14 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Setp up socket location
-bind "unix://#{shared_dir}/tmp/sockets/CrjCom-puma.sock"
+bind "unix://#{app_dir}/tmp/sockets/CrjCom-puma.sock"
 
 # Logging
-# stdout_redirect "#{}/log/puma.stdout.log", "#{release_path}/log/puma.stderr.log", true
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_path}/tmp/pids/puma.pid"
-state_path "#{shared_path}/tmp/pids/puma.state"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
 
 on_worker_boot do
   require "active_record"
