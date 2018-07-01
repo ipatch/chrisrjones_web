b0c491754734751a8e8360d2563e5a85e9a70c61
Chris Jones <chris.r.jones.1983@gmail.com>
2018-06-30 20:04:52 -0500

    bump rails to 4.2.10, and switch JS Runtime to Nodejs

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+# set the JS runtime to Nodejs
+ENV['EXECJS_RUNTIME'] = 'Node'
+
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
