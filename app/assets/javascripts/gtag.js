95ca7cce2216e72b1c0ccce2bc8cfb660aade06f
Chris Jones <chris.r.jones.1983@gmail.com>
2019-01-02 15:36:42 -0600

    formatting updates for `public.html.erb`, `application.html.erb`, `_navigation.html.erb` `_footer.html.erb`, and document `gtag.js`

--- app/assets/javascripts/gtag.js
+++ app/assets/javascripts/gtag.js
@@ -1,12 +1,22 @@
-// Global Site Tag (gtag.js) - Google Analytics
+// HTML 5 version
+//
+// <!-- Global Site Tag (gtag.js) - Google Analytics -->
 // <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
 // <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'GA_TRACKING_ID');
+// </script>
+
+
+// Javascript version
+//
 (function () {
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  // gtag('config', 'GA_TRACKING_ID');
   gtag('config', 'UA-21754484-2');
-// </script>
 }());
