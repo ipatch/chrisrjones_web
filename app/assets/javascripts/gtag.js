2643842ac825a368aaf721f9dbe974694bfb4094
Chris Jones <chris.r.jones.1983@gmail.com>
2019-01-02 15:04:50 -0600

    update `gtag.js` script

--- app/assets/javascripts/gtag.js
+++ app/assets/javascripts/gtag.js
@@ -1,10 +1,12 @@
 // Global Site Tag (gtag.js) - Google Analytics
-<script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
-<script>
+// <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+// <script>
+(function () {
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   // gtag('config', 'GA_TRACKING_ID');
   gtag('config', 'UA-21754484-2');
-</script>
+// </script>
+}());
