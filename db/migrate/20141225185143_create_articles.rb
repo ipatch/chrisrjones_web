011df39111a89ca7ef1414bcf68ec0c0f245f6a4
Chris Jones <chris.r.jones.1983@gmail.com>
2019-03-20 15:24:40 -0500

    add a `created_by` column to the `articles` table

--- db/migrate/20141225185143_create_articles.rb
+++ db/migrate/20141225185143_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :text
+      t.string :created_by
 
       t.timestamps null: false
     end
