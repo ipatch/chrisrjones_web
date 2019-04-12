245e60d637526bf3c3d6f412741e7764a632ea31
Chris Jones <chris.r.jones.1983@gmail.com>
2019-04-12 11:01:39 -0500

    hotfix, set log level to warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -43,7 +41,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -101,5 +99,4 @@ Rails.application.configure do
     :enable_starttls_auto => true,
     openssl_verify_mode: 'none'
   }
-
 end
