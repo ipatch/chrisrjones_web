02501057472aeaa1f6a8f5edc3bee2d18ab0143a
Chris Jones <chris.r.jones.1983@gmail.com>
2019-05-20 14:24:52 -0500

    setup `boot.rb` to use `frozen_string_literal` magic comment, and disable output buffering for working with docker-compose

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+# disable output buffering
+$stdout.sync = true
+
 # Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('Gemfile', __dir__)
 
 # set the JS runtime to Nodejs
 ENV['EXECJS_RUNTIME'] = 'Node'
