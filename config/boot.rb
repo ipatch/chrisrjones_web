00cf5ea243c893bca171a0912d8abffc73faf3ac
chris <chris.r.jones.1983@gmail.com>
2023-07-16 16:27:28 -0500

    update `boot.rb` file to work with next available port if one is already in use ie. use 3001

--- config/boot.rb
+++ config/boot.rb
@@ -9,12 +9,44 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 # set the JS runtime to Nodejs
 ENV['EXECJS_RUNTIME'] = 'Node'
 
+# Find the next available port starting from 3001
+require 'socket'
+
+def next_available_port
+  port = 3001
+  while port <= 65535
+    return port if port_available?(port)
+    port += 1
+  end
+  nil
+end
+
+# Check if a specific port is available
+def port_available?(port)
+  begin
+    server = TCPServer.new('127.0.0.1', port)
+    server.close
+    true
+  rescue Errno::EADDRINUSE
+    false
+  end
+end
+
+# Set the Rails server port to the next available port
+port = next_available_port
+if port
+  puts "Using port #{port}..."
+  ENV['PORT'] = port.to_s
+else
+  puts "No available ports found, using default 3000..."
+end
+
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
 require 'rails/command'
 require 'rails/commands/server/server_command'
 
-# DEV > `rails s` over LAN
+# DEV / `rails s` over LAN
 # require 'rails/commands/server' # NOTE: does not work with rails ≥ 5.1
 module Rails
   # setup Server class for overriding default `rails s` options
@@ -24,3 +56,4 @@ module Rails
     end
   end
 end
+
