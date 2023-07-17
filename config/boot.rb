77fc6ebd5b6b4269d721153fd546f80258630923
chris <chris.r.jones.1983@gmail.com>
2023-07-16 22:04:32 -0500

    setup app to serve on local ipv4 address and find next avail port ✅

--- config/boot.rb
+++ config/boot.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
+#
+# NOTE: ipatch
+# https://guides.rubyonrails.org/v5.2/initialization.html
 
-# disable output buffering
-$stdout.sync = true
-
-# Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-# set the JS runtime to Nodejs
-ENV['EXECJS_RUNTIME'] = 'Node'
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # Find the next available port starting from 3001
 require 'socket'
 
 def next_available_port
-  port = 3001
+  port = 3000
+  # Get the local IP address dynamically
+  local_ip = Socket.ip_address_list.detect { |iface| iface.ipv4_private? }.ip_address
   while port <= 65535
-    return port if port_available?(port)
+    return port if port_available?(local_ip, port)
     port += 1
   end
   nil
 end
 
 # Check if a specific port is available
-def port_available?(port)
+def port_available?(ip, port)
   begin
-    server = TCPServer.new('127.0.0.1', port)
+    server = TCPServer.new(ip, port)
     server.close
     true
   rescue Errno::EADDRINUSE
@@ -37,23 +37,7 @@ port = next_available_port
 if port
   puts "Using port #{port}..."
   ENV['PORT'] = port.to_s
+  ENV['HOST'] = '0.0.0.0'
 else
   puts "No available ports found, using default 3000..."
 end
-
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
-
-require 'rails/command'
-require 'rails/commands/server/server_command'
-
-# DEV / `rails s` over LAN
-# require 'rails/commands/server' # NOTE: does not work with rails ≥ 5.1
-module Rails
-  # setup Server class for overriding default `rails s` options
-  class Server
-    def default_options
-      super.merge(Host: '0.0.0.0', Port: 3000)
-    end
-  end
-end
-
