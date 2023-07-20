4cbec418881c69fe733c91571ee59698fa0de851
chris <chris.r.jones.1983@gmail.com>
2023-07-20 14:41:51 -0500

    checkin a execjs.rb initializer for exp with different JS runtimes #wip

---
+++ config/initializers/execjs.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+# NOTE: ipatch,
+# define & experiment with custom non default JS runtimes
+# https://www.rubydoc.info/gems/execjs/2.8.1
+
+# TODO: ipatch, presently this does not work
+
+module ExecJS
+  module Runtimes
+    if ENV['EXECJS_BUN']
+      Bun = ExternalRuntime.new(
+        name: "bun",
+        command: ["bun run"],
+        # runner_path: ExecJS.root + "/home/capin/.bun/bin/bun",
+        encoding: "UTF-8"
+      )
+    end
+  end
+end
