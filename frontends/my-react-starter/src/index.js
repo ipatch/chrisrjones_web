3acc989b3cb668eef0ffaf347ed3ccc0a3125c0a
chris <chris.r.jones.1983@gmail.com>
2023-07-31 21:19:14 -0500

    #react setup root route and errorElement in index.js

--- frontends/my-react-starter/src/index.js
+++ frontends/my-react-starter/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react'; // required becuz JSX
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import App from './App';
+import ErrorPage from './error-page.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+]);
 
 const container = document.getElementById('app');
 if (container instanceof HTMLElement) {
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 } else {
   console.error('element with id "app" not found');
 }
