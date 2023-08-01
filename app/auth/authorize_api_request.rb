bc66632d44e51cdfb629201d0312fbce2bdb91ac
chris <chris.r.jones.1983@gmail.com>
2023-08-01 16:36:20 -0500

    #rails update authorize_api_request

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -17,27 +17,20 @@ class AuthorizeApiRequest
   attr_reader :headers
 
   def user
-    # check if user is in the database
-    # memoize user object
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-    # handle user not found
   rescue ActiveRecord::RecordNotFound => e
-    # raise custom error
-    raise(
-      ExceptionHandler::InvalidToken,
-      ("#{Message.invalid_token} #{e.message}")
-    )
+    raise ExceptionHandler::InvalidToken, "#{Message.invalid_token} #{e.message}"
   end
 
-  # decode authentication token
   def decoded_auth_token
     @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
   end
 
-  # check for token in `Authorization` header
   def http_auth_header
-    return headers['Authorization'].split(' ').last if headers['Authorization'].present?
+    if @headers['Authorization'].present?
+      return @headers['Authorization'].split(' ').last
+    end
 
-    raise(ExceptionHandler::MissingToken, Message.missing_token)
+    raise ExceptionHandler::MissingToken, Message.missing_token
   end
 end
