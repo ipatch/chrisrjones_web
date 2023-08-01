0439a704951d7cd428df36e1e814b7220a0da541
chris <chris.r.jones.1983@gmail.com>
2023-08-01 10:09:44 -0500

    #react add route for about page

--- frontends/my-react-starter/src/index.js
+++ frontends/my-react-starter/src/index.js
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import App from './App';
 import ErrorPage from './error-page.jsx';
+import { AboutPage } from './about-page.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />
+  },
+  {
+    path: '/about',
+    element: <AboutPage />
   },
 ]);
 
