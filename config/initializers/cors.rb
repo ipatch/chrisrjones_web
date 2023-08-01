9105208f61a4ab323e354be39664df511405bc9b
chris <chris.r.jones.1983@gmail.com>
2023-08-01 16:35:05 -0500

    #rails update cors initializers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8080'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true # allow requests with cred ie. cookies
   end
 end
 
