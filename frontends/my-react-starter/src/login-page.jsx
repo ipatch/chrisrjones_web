0e1802951c044d5e96fe35d13db0e61571f17d17
chris <chris.r.jones.1983@gmail.com>
2023-08-01 11:53:25 -0500

    #react #rails have very basic login working with react rails using cors

--- frontends/my-react-starter/src/login-page.jsx
+++ frontends/my-react-starter/src/login-page.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Navbar } from './components/Navbar.jsx'
 import './styles.css';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+  try {
+    // send POST request to localhost:3000/auth/login
+    const response = await axios.post('http://localhost:3000/api/auth/login', {
+      email: email,
+      password: password,
+    });
+
+    // if login good, handle response
+    console.log(response.data);
+
+  } catch (error) {
+    // handle error
+    console.error(error);
+    }
+  };
+
     return(
       <>
       <Navbar />
       <div className='login-page-container'>
         <h1>login</h1>
-        <form action='#'>
+        <form onSubmit={handleLogin}>
           <div>
           <label htmlFor='fname'>username:</label>
-          <input type='text' id='uname' name='fname' />
+          <input type='text' id='uname' value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div>
           <label htmlFor='password'>password:</label>
-          <input type='text' id='password' name='password' />
+          <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
-          <input type='submit' value='submit' />
+          <input type='submit' value='login' />
         </form>
       </div>
       </>
