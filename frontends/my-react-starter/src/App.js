bdbc0fa0e23d6fb0e496a1247c170a7d3a283271
chris <chris.r.jones.1983@gmail.com>
2023-08-02 15:05:00 -0500

    #react convert app.js to a functional component

--- frontends/my-react-starter/src/App.js
+++ frontends/my-react-starter/src/App.js
@@ -2,24 +2,19 @@ import React from 'react';
 import { Navbar, } from './components/Navbar.jsx';
 import { Counter, } from './components/Counter.js';
 import PlaceholderBlogPost from './components/PlaceholderBlogPost.jsx';
-import './styles.css';
 import '../node_modules/modern-normalize/modern-normalize.css';
-// import { hot } from 'react-hot-loader';
+import './styles.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <main>
-          <Counter />
-          <PlaceholderBlogPost />
-        </main>
-      </>
-    );
-  }
-}
+const AppFunc = () => {
+  return (
+    <>
+    <Navbar />
+    <main>
+      <Counter />
+      <PlaceholderBlogPost />
+    </main>
+ </>
+  );
+};
 
-// preserve app state during hot reloading
-// export default hot(module)(App);
-export default App;
+export { AppFunc, };
