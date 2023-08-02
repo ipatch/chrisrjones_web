b84b68d913666a0a36c2af6829cc5fc60d63866e
chris <chris.r.jones.1983@gmail.com>
2023-08-02 15:21:25 -0500

    scaffold out a articles-page for pulling blog posts from rails api

---
+++ frontends/my-react-starter/src/articles-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ArticlesPage = () => {
+  return(
+    <>
+    <div className='articles-page-container'>
+    <h1>article 1</h1>
+    <p>todo pull in articles from rails api</p>
+    </div>
+    </>
+  );
+};
+
+export { ArticlesPage };
