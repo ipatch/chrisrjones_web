5d21d3e36f4715401bae4dd243d13ba407741d69
chris <chris.r.jones.1983@gmail.com>
2023-08-05 18:14:41 -0500

    #fe #react swap out placeholderblogpost for articlespage component

--- frontends/my-react-starter/src/App.js
+++ frontends/my-react-starter/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Navbar, } from './components/Navbar.jsx';
 import { Counter, } from './components/Counter.js';
-import PlaceholderBlogPost from './components/PlaceholderBlogPost.jsx';
+// import PlaceholderBlogPost from './components/PlaceholderBlogPost.jsx';
+import { ArticlesPage } from './articles-page.jsx';
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './styles.css';
 
@@ -11,7 +12,7 @@ const AppFunc = () => {
     <Navbar />
     <main>
       <Counter />
-      <PlaceholderBlogPost />
+      <ArticlesPage />
     </main>
  </>
   );
