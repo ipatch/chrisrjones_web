2b3ac5e928d7b254d25a888e91981820b18c8c95
chris <chris.r.jones.1983@gmail.com>
2023-08-18 18:36:10 -0500

    #rails #rubocop resolve offenses in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,41 @@
 # frozen_string_literal: true
 
+# SessionsController handles user authentication and session management.
 class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password]) && user.confirmed == true
-      session[:user_id] = user.id
-      redirect_to root_url, notice: 'Logged in!'
+    user = find_user_by_email
+
+    if valid_user?(user)
+      log_in_user(user)
+      redirect_to root_url, notice: t('notices.logged_in')
     else
-      flash.now.alert = 'Email or password is invalid'
-      render 'new'
+      handle_invalid_credentials
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Logged out!'
+    redirect_to root_url, notice: t('notices.logged_out')
+  end
+
+  private
+
+  def find_user_by_email
+    User.find_by(email: params[:email])
+  end
+
+  def valid_user?(user)
+    user&.authenticate(params[:password]) && user&.confirmed
+  end
+
+  def log_in_user(user)
+    session[:user_id] = user.id
+  end
+
+  def handle_invalid_credentials
+    flash.now.alert = t('alerts.invalid_credentials')
+    render 'new'
   end
 end
