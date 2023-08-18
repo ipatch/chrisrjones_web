4b23ed3683411d5ce5c1ccd88386761aa907b474
chris <chris.r.jones.1983@gmail.com>
2023-08-18 14:32:05 -0500

    #rails add i18n en.yml file for mailer text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
+# represents the mailer for sending emails to users
 class UserMailer < ApplicationMailer
   default from: 'no-reply@chrisrjones.com'
   default to: 'me@chrisrjones.com'
 
   def new_message(message)
     @message = message
-    mail(subject: "[chrisrjones.com] #{message.subject}")
+    mail(subject: t('user_mailer.new_message.subject', subject: message.subject))
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'Password Reset'
+    mail(to: user.email, subject: t('user_mailer.password_reset.subject'))
   end
 
   def confirmation(user)
     @user = user
-    mail to: user.email, subject: 'Confirm Account'
+    mail(to: user.email, subject: t('user_mailer.confirmation.subject'))
   end
 end
