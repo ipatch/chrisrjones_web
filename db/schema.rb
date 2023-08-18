7dbeda1d366997184e18da7278e7667e79911a14
chris <chris.r.jones.1983@gmail.com>
2023-08-18 14:19:42 -0500

    #rails run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_18_190909) do
+ActiveRecord::Schema.define(version: 2023_08_18_191519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_08_18_190909) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "meta_description"
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
 
   create_table "attachments", force: :cascade do |t|
