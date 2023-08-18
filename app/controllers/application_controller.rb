b219ba6342e268158ed63a5799fc6aa150efeb8f
chris <chris.r.jones.1983@gmail.com>
2023-08-18 18:43:43 -0500

    #rails #rubocop resolve offenses in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# ApplicationController serves as the parent class for all controllers in the application.
+# It contains common methods and filters that are shared across multiple controllers.
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
 
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       true
     else
-      flash[:notice] = 'Please log in.'
+      flash[:notice] = t('notices.please_login')
       redirect_to(controller: 'access', action: 'login')
       false # halts the before_action
     end
@@ -28,6 +30,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to login_url, alert: 'Not authorized' if current_user.nil?
+    redirect_to login_url, alert: t('alerts.not_authorized')
   end
 end
