d8159d6b42e9e243f589f0f374c01f1d2a05662c
chris <chris.r.jones.1983@gmail.com>
2023-08-19 16:49:17 -0500

    #rails resolve rubocop offenses in password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,16 +11,16 @@ class PasswordResetsController < ApplicationController # :nodoc:
   def create
     user = User.find_by(params[:email])
     user&.send_password_reset
-    redirect_to root_url, notice: 'Email sent with password reset instructions.'
+    redirect_to root_url, notice: t('notices.password_reset_instructions')
   end
 
   def update
     @user = User.find_by!(password_reset_token: params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_password_reset_path, alert: 'Password reset has expired.'
+      redirect_to new_password_reset_path, alert: t('alerts.password_reset_expired')
       # elseif @user.update_attributes(params[:user])
     elsif @user.update(password_params)
-      redirect_to root_url, notice: 'Password has been reset!'
+      redirect_to root_url, notice: t('notices.password_been_reset')
     else
       render :edit
     end
