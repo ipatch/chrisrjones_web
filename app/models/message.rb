381542b951f5e48e9f52b71ae4d292b80303341f
chris <chris.r.jones.1983@gmail.com>
2023-08-21 20:30:31 -0500

    #rails update message model to inherit from apprecord

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-class Message # :nodoc:
+# a class for holding messages sent via the contact form
+class Message < ApplicationRecord
   include ActiveModel::Validations
   include ActiveModel::Conversion
+  include ActiveModel::Model # Include this module to provide necessary Active Model methods
+  include ActiveModel::Attributes
   extend ActiveModel::Naming
 
   attr_accessor :name, :email, :subject, :body
@@ -10,11 +13,11 @@ class Message # :nodoc:
   validates :name, :email, :subject, :body, presence: true
   validates :email, format: { with: /.+@.+\..+/ }, allow_blank: true
 
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
-  end
+  # def initialize(attributes = {})
+  #   attributes.each do |name, value|
+  #     send("#{name}=", value)
+  #   end
+  # end
 
   def persisted?
     false
