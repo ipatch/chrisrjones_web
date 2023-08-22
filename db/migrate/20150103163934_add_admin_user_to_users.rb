b0894ff97af6ac80ea3ca69af3246315c6ce9b6f
chris <chris.r.jones.1983@gmail.com>
2023-08-22 13:04:34 -0500

    #rails add top level comment

--- db/migrate/20150103163934_add_admin_user_to_users.rb
+++ db/migrate/20150103163934_add_admin_user_to_users.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# migration for adding admin priv for certain users
 class AddAdminUserToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :admin_user, :boolean, default: false
