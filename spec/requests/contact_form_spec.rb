e20a4484c596250189220f9f16fb65774a4f2bc4
chris <chris.r.jones.1983@gmail.com>
2023-08-30 12:10:15 -0500

    #rspec work on contact_form_spec

--- spec/requests/contact_form_spec.rb
+++ spec/requests/contact_form_spec.rb
@@ -3,7 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'ContactForms' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /contact' do
+    it 'responds with a success status' do
+      get '/contact'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the new template' do
+      get '/contact/new'
+      expect(response).to render_template(:new)
+    end
   end
 end
