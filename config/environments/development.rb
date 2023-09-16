5f25c7634f00bd2322955255d8c36d5fc201d8c9
chris <chris.r.jones.1983@gmail.com>
2023-09-16 14:30:05 -0500

    don't cache assets in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.assets.configure { |env| env.cache = ActiveSupport::Cache.lookup_store(:null_store) }
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large number of complex assets.
   config.assets.debug = true
